369647a36c0c7bc0dd5839bff5dc6f2e96ea5b42
PrabhavPandey <prabhavp380@gmail.com>
2020-05-23 15:10:25 +0530

    Update phone.py

--- phone.py
+++ phone.py
@@ -1,31 +1,24 @@
-import phonenumbers 
 
-print("enter your phone number with the country code")
-print("for example: +91xxxxxxxxxx")
-x=str(input("number with country code: "))
+print("---------------------------------------------------------\nWELCOME TO THE EMAIL VALIDATION TOOL\n---------------------------------------------------------")
 
-if len(x)==13:
-    from phonenumbers import geocoder 
-    phone_number = phonenumbers.parse(x) 
-    print("country is: ",geocoder.description_for_number(phone_number,'en'))
+print("Please begin by entering your E-Mail body text, sender's E-Mail ID and any phone numbers given below")
+body = str(input("Copy and paste the body of the E-Mail here:  "))
+sender = str(input("Enter the E-Mail ID of the sender:  "))
+number = int(input("Enter any phone numbers in the mail:  "))
 
-    from phonenumbers import carrier 
-    service_provider = phonenumbers.parse(x) 
-    print("carrier is: ",carrier.name_for_number(service_provider,'en'))
-else:
-    print("enter valid number")
+def num(number):
+     
+     import phonenumbers
+     print("Enter your phone number with the country code: ")
+     print("For example: +91xxxxxxxxxx")
+     number=str(input("Number with country code: "))
 
+     if len(number)==13:
+         from phonenumbers import geocoder 
+         phone_number = phonenumbers.parse(number) 
+         print("Country is: ",geocoder.description_for_number(phone_number,'en'))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+         from phonenumbers import carrier 
+         service_provider = phonenumbers.parse(number) 
+         print("Carrier is: ",carrier.name_for_number(service_provider,'en'))
+        
