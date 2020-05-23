c4d27170638271e7355c0c275fbdc9ed8c74ba3f
PrabhavPandey <prabhavp380@gmail.com>
2020-05-23 11:56:13 +0530

    Update CreditCard.py

--- CreditCard.py
+++ CreditCard.py
@@ -2,11 +2,22 @@
 print("---------------------------------------------------------\nWELCOME TO THE CREDIT CARD VALIDATION TOOL\n---------------------------------------------------------")
 
 n = int(input("Please begin by entering the Credit Card Number:  "))
+string = str(n)
 a = str(n)
 if len(a) ==16:
      
      print("Yoww")
-     p = str(input("Enter the mentioned publisher of your card (present in the bottom right corner) : "))
+     p = str(input("Enter the mentioned publisher of your card (present in the bottom right corner) in full CAPITALS: "))
+     if p == "VISA":
+          for i in string:
+               if i[0] == 4:
+                    check1 = 1
+               else:
+                    check1 = 0
+                    
+            
+          
+          
      
 #Module calling
 else:
