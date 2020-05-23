4d387a4442c7c41b411fe73ac515a6f97d40f2f2
PrabhavPandey <prabhavp380@gmail.com>
2020-05-23 14:54:46 +0530

    Update CreditCard.py

--- CreditCard.py
+++ CreditCard.py
@@ -3,24 +3,34 @@ print("---------------------------------------------------------\nWELCOME TO THE
 
 n = int(input("Please begin by entering the Credit Card Number:  "))
 string = str(n)
-a = str(n)
+Master_Card = ["51","52","53","54","55"]
+American_Express = ["34","37"]
+
+#Publisher Check
 if len(a) ==16:
-     
      print("Yoww")
      p = str(input("Enter the mentioned publisher of your card (present in the bottom right corner) in full CAPITALS: "))
      if p == "VISA":
-          for i in string:
-               if i[0] == 4:
-                    check1 = 1
-               else:
-                    check1 = 0
-                    
-            
-          
-          
+          if string[0] == 4:
+               check1 = 1
+          else:
+               check1 = 0
+     elif p == "MASTER CARD":
+          if string[0:2] in Master_Card:
+               check1 = 1
+          else:
+               check1 = 0
+     elif p == "AMERICAN EXPRESS":
+          if string[0:2] in American_Express:
+               check1 = 1
+          else:
+               check1 = 0
+if check1 == 0:
+     print("Your card number does not belong to the specified publisher, it possibly a fraudulent card")
+continue          
      
 #Module calling
 else:
-     print("Please enter a valid Credit Card Number, it must be a 16 digit number")
+     print("Please enter a valid Credit Card Number, it must be a 16 digit number for the tool to work!")
 
 
