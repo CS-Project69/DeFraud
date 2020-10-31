5370377ae119669fbae0f29d1c1c3b1584965b6f
PrabhavPandey <prabhavp380@gmail.com>
2020-10-31 10:43:46 +0530

    Update Kaggle - Reader.py

--- Kaggle - Reader.py
+++ Kaggle - Reader.py
@@ -2,48 +2,47 @@ import numpy as np
 import pandas as pd 
 import matplotlib.pyplot as plt
 
-file = input("Enter name of the file with transactional entries: ")
-data = pd.read_csv(file)
+def kaggle1():
+     file = input("Enter name of the file with transactional entries: ")
+     data = pd.read_csv(file)
 
 
-#Analysis
-fraud = data[data['Class'] == 1]
-valid = data[data['Class'] == 0]
+     #Analysis
+     fraud = data[data['Class'] == 1]
+     valid = data[data['Class'] == 0]
 
-print(
+     Fraction = len(fraud)/float(len(valid))
+     print("Fraction of fraudulent entries: ",Fraction) 
+     print("Fraud Cases: {}".format(len(fraud)))
+     print("Valid Transactions: {}".format(len(valid)))
 
-outlierFraction = len(fraud)/float(len(valid)) 
-print("Fraction of fraudulent entries: ",outlierFraction) 
-print("Fraud Cases: {}".format(len(fraud)))
-print("Valid Transactions: {}".format(len(valid)))
+     #Checking
+     list1 = []
+     for i in list1:
+          list1.append(fraud["Time"])
+     print (list1)
 
-#Checking
-list1 = []
-for i in list1:
-     list1.append(fraud["Time"])
-print (list1)
+     inp = int(input("Enter the time code of the transaction you want to check: "))
+     if inp in list1:
+          print("Fraud")
+          
+     else:
+          print("Not Fraud")
 
-inp = int(input("Enter the time of the transaction you want to check: "))
-if inp in list1:
-     print("Fraud")
-     
-else:
-     print("Not Fraud")
+     #Graphical Plot
+     count_classes = pd.value_counts(data['Class'], sort=True)
+     count_classes.plot(kind='bar', rot=0)
+     plt.title("Distributed Transactions")
+     plt.xticks(range(2), ['Normal', 'Fraud'])
+     plt.xlabel("Class")
+     plt.ylabel("Frequency")
+     plt.show()
 
-#Graphical Plot
-count_classes = pd.value_counts(data['Class'], sort=True)
-count_classes.plot(kind='bar', rot=0)
-plt.title("Distributed Transactions")
-plt.xticks(range(2), ['Normal', 'Fraud'])
-plt.xlabel("Class")
-plt.ylabel("Frequency")
-plt.show()
+     #Details 
+     print("Details of the fraudulent transactions")
+     print("----------------------")
+     print(fraud.Amount.describe())
 
-#Details 
-print("Details of the fraudulent transactions")
-print("----------------------")
-print(fraud.Amount.describe())
-
-print("Details of valid transactions")
-print("----------------------")
-print(valid.Amount.describe())
+     print("Details of valid transactions")
+     print("----------------------")
+     print(valid.Amount.describe())
