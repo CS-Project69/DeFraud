4eb4fa77cb8f4a0b73101b63f922f2cd91a33a8d
PrabhavPandey <prabhavp380@gmail.com>
2020-12-30 23:09:42 +0530

    Update Kaggle - Reader.py

--- Kaggle - Reader.py
+++ Kaggle - Reader.py
@@ -1,12 +1,20 @@
 import numpy as np
 import pandas as pd 
-import matplotlib.pyplot as plt
+import csv
 
 def kaggle1():
      file = input("Enter name of the file with transactional entries: ")
      data = pd.read_csv(file)
 
+     list1 = []
+     
+     with open(file,"r") as f:
 
+          reader = csv.reader(f)
+          for row in reader:
+               if row[30] == "1":
+                    list1.append(row[0])
+                         
      #Analysis
      fraud = data[data['Class'] == 1]
      valid = data[data['Class'] == 0]
@@ -17,32 +25,10 @@ def kaggle1():
      print("Valid Transactions: {}".format(len(valid)))
 
      #Checking
-     list1 = []
-     for i in list1:
-          list1.append(fraud["Time"])
-     print (list1)
-
-     inp = int(input("Enter the time code of the transaction you want to check: "))
+     inp = input("Enter the time code of the transaction you want to check: ")
      if inp in list1:
-          print("Fraud")
+            print("Fraud")
           
      else:
           print("Not Fraud")
-
-     #Graphical Plot
-     count_classes = pd.value_counts(data['Class'], sort=True)
-     count_classes.plot(kind='bar', rot=0)
-     plt.title("Distributed Transactions")
-     plt.xticks(range(2), ['Normal', 'Fraud'])
-     plt.xlabel("Class")
-     plt.ylabel("Frequency")
-     plt.show()
-
-     #Details 
-     print("Details of the fraudulent transactions")
-     print("----------------------")
-     print(fraud.Amount.describe())
-
-     print("Details of valid transactions")
-     print("----------------------")
-     print(valid.Amount.describe())
+kaggle1()
