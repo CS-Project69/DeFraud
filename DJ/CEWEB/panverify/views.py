ca1dbe93c911c043b53d5d41307415125513eac7
CR7-RAMA <64270937+CR7-RAMA@users.noreply.github.com>
2021-01-14 02:01:09 +0530

    more changes

--- DJ/CEWEB/panverify/views.py
+++ DJ/CEWEB/panverify/views.py
@@ -13,9 +13,9 @@ def about(request):
     return render(request,'about.html')
 def contact(request):
     return render(request,'contact us.html')
-captcha=''
+
 def captcha1():
-    global captcha
+    captcha=''
     l4=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9","0"]
     for i in range(5):
         y=random.randint(0,61)
